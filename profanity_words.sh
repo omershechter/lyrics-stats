34279f854552f99869412b1ecee0ffb7ce5efd8f
Omer Shechter <omer.shechter@post.idc.ac.il>
2021-12-23 14:53:16 +0200

    Add script to print the number of profanity words per year using facebook's bad words list.

---
+++ profanity_words.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for year in $(seq 1960 2016); do
+	num_dirty_words=0
+	for word in $(cat facebook-bad-words-list_comma-separated-text-file_2021_01_18.txt); do
+		year_songs=$(cat lyrics/"${year}"*|tr ' ' '\n'|tr -d '[:punct:]'|tr '[:lower:]' '[:upper:]'|grep -wc "${word}")
+		num_dirty_words=$((num_dirty_words+year_songs))
+	done
+	echo "${year}", "${num_dirty_words}"
+done
